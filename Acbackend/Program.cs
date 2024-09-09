afe00fd31e060ac41bda966e8b2d4be17003e5e2
StefanStenStark <110032501+StefanStenStark@users.noreply.github.com>
2024-09-09 13:07:08 +0200

    Feat: added one line of code to make the api work

--- Acbackend/Program.cs
+++ Acbackend/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ActionCardAppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ActionCardAppContext") ?? throw new InvalidOperationException("Connection string 'ActionCardAppContext' not found.")));
@@ -13,38 +12,16 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
--- Acbackend/models/User.cs
+++ Acbackend/models/User.cs
@@ -1,5 +1,5 @@
 public class User(){
     public int Id {get; set;}
-    public string Name  {get; set;}
-    public string Stars {get; set;}
+    public string? Name  {get; set;}
+    public string? Stars {get; set;}
 }
