91d27f635e3601312c18882937414ff4d1ea1525
StefanStenStark <110032501+StefanStenStark@users.noreply.github.com>
2024-09-09 09:49:36 +0200

    first commit

---
+++ Acbackend/Data/ActionCardAppContext.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+    public class ActionCardAppContext : DbContext
+    {
+        public ActionCardAppContext (DbContextOptions<ActionCardAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ActionCard> ActionCard { get; set; } = default!;
+    }
---
+++ Acbackend/models/ActionCard.cs
@@ -0,0 +1,6 @@
+public class ActionCard(){
+    public int? Id {get; set;}
+    public string? Type {get; set;}
+    public string? Title {get; set;}
+    public string? Instruction {get; set;}
+}
