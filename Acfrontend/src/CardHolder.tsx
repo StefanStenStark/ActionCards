97678097b015cb2c7a2b2546c105be4d0ebf94d7
StefanStenStark <110032501+StefanStenStark@users.noreply.github.com>
2024-09-10 09:36:42 +0200

    feat: adding cards

--- Acfrontend/src/CardHolder.tsx
+++ Acfrontend/src/CardHolder.tsx
@@ -2,6 +2,7 @@ export default function CardHolder({ title }: { title: string }) {
   return (
     <section className="main-section">
       <h2>{title}</h2>
+      <img src="/ActionCard.jpg" className="cardholder-card" />
     </section>
   );
 }
