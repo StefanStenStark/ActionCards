acd3fd1c4647c9989fe92d88b43f95690e9ee803
StefanStenStark <110032501+StefanStenStark@users.noreply.github.com>
2024-09-10 11:20:13 +0200

    feat: made clicking card show front

--- Acfrontend/src/CardHolder.tsx
+++ Acfrontend/src/CardHolder.tsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
+
+function Card() {
+  const [showFront, setShowFront] = useState(false);
+  function handleClick() {
+    setShowFront(true);
+  }
+
+  return (
+    <div className="cardholder-card" onClick={handleClick}>
+      {!showFront ? (
+        <img src="/ActionCard.jpg" className="cardholder-card-image" />
+      ) : (
+        <>
+          <h1>clean oven</h1>
+          <p>clean the oven as good as you can</p>
+          <button>Accept</button>
+        </>
+      )}
+    </div>
+  );
+}
+
 export default function CardHolder({ title }: { title: string }) {
   return (
     <section className="main-section">
       <h2 className="cardholder-title">{title}</h2>
       <div className="cardholder-holder">
-        <img src="/ActionCard.jpg" className="cardholder-card" />
-        <img src="/ActionCard.jpg" className="cardholder-card" />
-        <img src="/ActionCard.jpg" className="cardholder-card" />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </section>
   );
