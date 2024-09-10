32aef3f92edc4290e5ef883214bf75ee30116f9f
StefanStenStark <110032501+StefanStenStark@users.noreply.github.com>
2024-09-10 12:02:50 +0200

    feat: adding cors

--- Acbackend/Program.cs
+++ Acbackend/Program.cs
@@ -1,27 +1,37 @@
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost5173",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddDbContext<ActionCardAppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ActionCardAppContext") ?? throw new InvalidOperationException("Connection string 'ActionCardAppContext' not found.")));
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowLocalhost5173");
+
+app.UseAuthorization();
+app.MapControllers();
+
 app.Run();
-
-
