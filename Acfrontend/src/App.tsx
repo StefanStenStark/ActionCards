e325f1b75325e709e6122462df1526fab3767176
StefanStenStark <110032501+StefanStenStark@users.noreply.github.com>
2024-09-16 09:21:18 +0200

    feat: adding the landing page as the landing page

--- Acfrontend/src/App.tsx
+++ Acfrontend/src/App.tsx
@@ -4,11 +4,13 @@ import Navbar from "./pages/Navbar";
 import DeckPage from "./pages/Deckpage";
 import ProfilePage from "./pages/ProfilePage";
 import ActionCardsPage from "./pages/ActionCardsPage";
+import LandingPage from "./pages/LandingPage";
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/ActionCards" element={<ActionCardsPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/Deck" element={<DeckPage />} />
